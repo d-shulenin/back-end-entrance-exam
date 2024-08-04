74fd9b06ebdc0af834e4762c6089ac4eb1a7ff11
Daniil Shulenin <dannnikk70787@yandex.ru>
2024-08-05 02:30:11 +0300

    docs: adjust readme

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import { cleanEnv, str, num } from "envalid";
 
 export const env = cleanEnv(process.env, {
-    API_BASE_URL: str(),
+    API_BASE_URL: str({ default: "https://the-one-api.dev/v2" }),
     API_KEY: str(),
     SERVER_PORT: num({ default: 3000 })
 });
