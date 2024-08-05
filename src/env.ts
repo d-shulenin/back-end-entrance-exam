ed8e1165c946d14011bbe398552550f6328a581a
Daniil Shulenin <dannnikk70787@yandex.ru>
2024-08-05 04:44:53 +0300

    ci: vercel p.7

--- src/env.ts
+++ src/env.ts
@@ -3,5 +3,5 @@ import { cleanEnv, str, num } from "envalid";
 export const env = cleanEnv(process.env, {
     API_BASE_URL: str({ default: "https://the-one-api.dev/v2" }),
     API_KEY: str(),
-    SERVER_PORT: num({ default: 3000 })
+    SERVER_PORT: num({ default: 3005 })
 });
